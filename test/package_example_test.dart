e82ff59b89f96cdb5b2c2f453513a4c561920937
Mohit-Nexever <mohitk.nexever@gmail.com>
2024-08-13 15:43:14 +0530

    adding code

--- test/package_example_test.dart
+++ test/package_example_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:package_example/package_example.dart';
+import 'package:package_example_new/package_example.dart';
 
 void main() {
   test('adds one to input values', () {
